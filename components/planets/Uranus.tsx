58ce6ea07f93f021dff73186e9c93938ae049c31
Gerard <gerard@protofy.xyz>
2023-09-06 12:28:32 +0200

    adds textures to planets

--- components/planets/Uranus.tsx
+++ components/planets/Uranus.tsx
@@ -1,33 +1,22 @@
 import React from 'react';
-import { useGLTF } from '@react-three/drei';
-
+import { useTexture } from "@react-three/drei";
+import * as THREE from 'three';
 
 export default function Model(props) {
-    const { nodes, materials } = useGLTF("/3d/uranus.gltf") as any;
-    return (
-      <group {...props} dispose={null}>
-        <group position={[0, 0, 0]}>
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Icosphere009.geometry}
-            material={materials.Uranus}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Icosphere009_1.geometry}
-            // material={materials["Atmosphere.008"]}
-          />
-          <mesh
-            castShadow
-            receiveShadow
-            geometry={nodes.Icosphere009_2.geometry}
-            // material={materials["Rings.001"]}
-          />
-        </group>
+  const planetTexture = useTexture("/textures/uranus.png")
+  const ringTexture = useTexture("/textures/uranus_rings.png")
+  return (
+    <group {...props} dispose={null}>
+      <group position={[0, 0, 0]}>
+        <mesh>
+          <sphereGeometry args={[5.5, 30, 30]} />
+          <meshStandardMaterial map={planetTexture} />
+        </mesh>
+        <mesh key="rings" rotation={[-Math.PI / 15, 0, 0]}>
+          <ringGeometry args={[5.6, 7.7, 50, 4]} />
+          <meshStandardMaterial map={ringTexture} side={THREE.DoubleSide} />
+        </mesh>
       </group>
-    );
-  }
-
-useGLTF.preload("/3d/uranus.gltf");
+    </group>
+  );
+}
--- components/planets/Venus.tsx
+++ components/planets/Venus.tsx
@@ -1,26 +1,16 @@
 import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import { useTexture } from "@react-three/drei";
 
 export default function Model(props) {
-  const { nodes, materials } = useGLTF("/3d/venus.gltf") as any;
+  const texture = useTexture('/textures/venus.jpg');
   return (
     <group {...props} dispose={null}>
-      <group position={[0, 0,0]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Icosphere010.geometry}
-        //   material={materials.Venus}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Icosphere010_1.geometry}
-        //   material={materials["Atmosphere.009"]}
-        />
+      <group position={[0, 0, 0]}>
+        <mesh>
+          <sphereGeometry args={[0.9, 30, 30]} />
+          <meshStandardMaterial map={texture} />
+        </mesh>
       </group>
     </group>
   );
 }
-
-useGLTF.preload("/3d/venus.gltf");

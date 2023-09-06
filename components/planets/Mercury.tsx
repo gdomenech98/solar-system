9c1cc17d639298fd458800b7d447f8cf9f045628
Gerard <gerard@protofy.xyz>
2023-09-06 10:27:11 +0200

    adds textures

--- components/planets/Mercury.tsx
+++ components/planets/Mercury.tsx
@@ -1,27 +1,18 @@
 
-import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import React from "react";
+import { useTexture } from "@react-three/drei";
 
 export default function Model(props) {
-  const { nodes, materials } = useGLTF("/3d/mercury.gltf") as any;
+  const texture = useTexture('/textures/mercury.jpg');
+
   return (
     <group {...props} dispose={null}>
       <group position={[0, 0, 0]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Icosphere003.geometry}
-        //   material={materials.Mercury}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Icosphere003_1.geometry}
-        //   material={materials["Atmosphere.003"]}
-        />
+        <mesh>
+          <sphereGeometry args={[0.6, 30, 30]} />
+          <meshStandardMaterial map={texture} />
+        </mesh>
       </group>
     </group>
   );
 }
-
-useGLTF.preload("/3d/mercury.gltf");

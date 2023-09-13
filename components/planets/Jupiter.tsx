c1ca0ec4b20dac7c7ec34224ef49cf923fd31769
Gerard <gerard@protofy.xyz>
2023-09-13 11:14:16 +0200

    adds atmosphere to venus and jupiter

--- components/planets/Jupiter.tsx
+++ components/planets/Jupiter.tsx
@@ -1,5 +1,7 @@
 import React, {} from "react";
 import { useTexture } from "@react-three/drei";
+import Atmosphere from "../Atmosphere";
+import * as THREE from "three";
 
 export default function Model(props) {
   const texture = useTexture("/textures/jupiter.jpg")
@@ -10,6 +12,7 @@ export default function Model(props) {
           <sphereGeometry args={[8, 30, 30]} />
           <meshStandardMaterial map={texture} />
         </mesh>
+        <Atmosphere radius={8.5} c={0.7} p={3}  color={new THREE.Color("#695431")} />
       </group>
     </group>
   );
--- components/planets/Venus.tsx
+++ components/planets/Venus.tsx
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
 import { useTexture } from "@react-three/drei";
+import Atmosphere from "../Atmosphere";
+import * as THREE from "three";
 
 export default function Model(props) {
   const texture = useTexture('/textures/venus.jpg');
@@ -10,6 +12,7 @@ export default function Model(props) {
           <sphereGeometry args={[1.5, 30, 30]} />
           <meshStandardMaterial map={texture} />
         </mesh>
+        <Atmosphere radius={2} c={0.7} p={5}  color={new THREE.Color("#5c4f00")} />
       </group>
     </group>
   );

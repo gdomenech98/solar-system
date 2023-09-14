c846788cfa497d320654b24aa9e7ad6c5e6991f4
Gerard <gerard@protofy.xyz>
2023-09-14 02:09:30 +0200

    add textures and add venus atmosphere

--- components/planets/Venus.tsx
+++ components/planets/Venus.tsx
@@ -5,6 +5,7 @@ import * as THREE from "three";
 
 export default function Model(props) {
   const texture = useTexture('/textures/venus.jpeg');
+  const atmosphereTexture = useTexture('/textures/venus_atmosphere.jpeg')
   return (
     <group {...props} dispose={null}>
       <group position={[0, 0, 0]}>
@@ -12,7 +13,13 @@ export default function Model(props) {
           <sphereGeometry args={[1.5, 30, 30]} />
           <meshStandardMaterial map={texture} />
         </mesh>
-        <Atmosphere radius={2} c={0.7} p={5}  color={new THREE.Color("#5c4f00")} />
+        <mesh receiveShadow={true}>
+          <sphereGeometry args={[1.6, 30, 30]}/>
+          <meshStandardMaterial map={atmosphereTexture} opacity={0.4} transparent={true}/>
+        </mesh>
+
+        {/* Add atmosphere texture to shader */}
+        {/* <Atmosphere radius={2} c={0.7} p={5}  color={new THREE.Color("#5c4f00")} /> */}
       </group>
     </group>
   );

4fb40838e55a52007ea103d4b080ae054bc9e107
Gerard <gerard@protofy.xyz>
2023-09-20 08:44:01 +0200

    adds helper function to zoomToObject

--- app/selectTest/page.tsx
+++ app/selectTest/page.tsx
@@ -6,7 +6,8 @@ import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import Floor from '@/components/Floor';
 import { Select, useSelect } from '@react-three/drei';
 import Cube from './components/Cube'
-import { Vector3, Box3 } from 'three';
+import { zoomToObject } from './zoomToObject';
+
 const ENABLE_FLOOR = false;
 
 export default function Test() {
@@ -14,36 +15,6 @@ export default function Test() {
     const controlsRef = useRef(null);
     const [selectedElement, setSelectedElement] = useState<any>();
 
-    function zoomToObject(object, camera, controls) { 
-        // Fits camera and controls to specific object
-        // SOURCE: https://discourse.threejs.org/t/camera-zoom-to-fit-object/936/3
-        let vFoV = camera.getEffectiveFOV();
-        let hFoV = camera.fov * camera.aspect;
-
-        let FoV = Math.min(vFoV, hFoV);
-        let FoV2 = FoV / 2;
-
-        let dir = new Vector3();
-        camera.getWorldDirection(dir);
-
-        let bs = object.geometry.boundingSphere;
-        let bsWorld = bs.center.clone();
-        object.localToWorld(bsWorld);
-
-        let th = FoV2 * Math.PI / 180.0;
-        let sina = Math.sin(th);
-        let R = bs.radius;
-        let FL = R / sina;
-        let cameraDir = new Vector3();
-        camera.getWorldDirection(cameraDir);
-        let cameraOffs = cameraDir.clone();
-        cameraOffs.multiplyScalar(-FL);
-        let newCameraPos = bsWorld.clone().add(cameraOffs);
-        camera.position.copy(newCameraPos);
-        camera.lookAt(bsWorld);
-        controls.target.copy(bsWorld);
-        controls.update();
-    }
     useEffect(() => {
         const camera = cameraRef.current;
         const controls = controlsRef.current

008527501e36d9e3c6d49336d5fce2c16324b635
Gerard <gerard@protofy.xyz>
2023-09-21 00:31:19 +0200

    adds loading bar as component

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,7 @@ import React, { Suspense, useRef } from 'react';
 import styles from './page.module.css';
 import { Canvas } from '@react-three/fiber';
 import Scene from '@/components/Scene';
-import { useProgress, Html } from '@react-three/drei';
-
-function LoadingBar() {
-  const { active, progress, loaded, total } = useProgress();
-
-  return (
-    <Html style={{ position: 'fixed', top: '10px', left: '50%', transform: 'translateX(-50%)', zIndex: 1000 }}>
-      <div style={{ width: '250px', height: '10px', background: '#555', borderRadius: '5px', overflow: 'hidden', boxShadow: '0px 0px 10px rgba(0,0,0,0.5)' }}>
-        <div style={{ width: `${progress}%`, height: '10px', background: 'white', borderRadius: '5px' }} />
-      </div>
-      <div style={{ marginTop: '8px', textAlign: 'center' }}>Loading {progress}%</div>
-    </Html>
-  );
-}
+import LoadingBar from '@/components/LoadingBar'
 
 export default function SolarSystem() {
   return (
---
+++ components/LoadingBar.tsx
@@ -0,0 +1,16 @@
+import { useProgress, Html } from '@react-three/drei';
+
+function LoadingBar() {
+    const { active, progress, loaded, total } = useProgress();
+    return (
+        <Html style={{ position: 'fixed', top: '10px', left: '50%', transform: 'translateX(-50%)', zIndex: 1000 }}>
+            <div style={{ width: '250px', height: '10px', background: '#555', borderRadius: '5px', overflow: 'hidden', boxShadow: '0px 0px 10px rgba(0,0,0,0.5)' }}>
+                <div style={{ width: `${progress}%`, height: '10px', background: 'white', borderRadius: '5px' }} />
+            </div>
+            <div style={{ marginTop: '8px', textAlign: 'center' }}>Loading {progress}%</div>
+        </Html>
+    );
+}
+
+
+export default LoadingBar;

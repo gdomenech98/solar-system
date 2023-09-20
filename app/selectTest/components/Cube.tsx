1be9643456c2738f34352a37341e3a7ec40cce67
Gerard <gerard@protofy.xyz>
2023-09-20 08:33:12 +0200

    centers camera target

--- app/selectTest/components/Cube.tsx
+++ app/selectTest/components/Cube.tsx
@@ -3,18 +3,7 @@ import { useThree } from '@react-three/fiber';
 import { useEffect, useRef } from 'react';
 
 export default function Cube(props) {
-    const selected = useSelect()
     const cubeRef = useRef();
-
-    const { camera } = useThree()
-    useEffect(() => {
-        const selectedElement = selected[0];
-        if(selectedElement){
-            camera.position.copy(selectedElement?.position);
-            camera.position.z += 5; // Adjust this to ensure the camera is not inside the object
-            camera.lookAt(selectedElement?.position);
-        }
-    }, [selected]);
 
     return (
         <mesh ref={cubeRef} receiveShadow={true} {...props}>
--- app/selectTest/page.tsx
+++ app/selectTest/page.tsx
@@ -1,15 +1,44 @@
 "use client"
-import React, { useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import styles from '../page.module.css';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import Floor from '@/components/Floor';
 import { Select, useSelect } from '@react-three/drei';
 import Cube from './components/Cube'
+import { Vector3, Box3 } from 'three';
 const ENABLE_FLOOR = false;
 
 export default function Test() {
-    const cameraRef = React.useRef(null);
+    const cameraRef = useRef(null);
+    const controlsRef = useRef(null);
+    const [selectedElement, setSelectedElement] = useState<any>();
+
+    useEffect(() => {
+        const camera = cameraRef.current;
+        const controls = controlsRef.current;
+        if (selectedElement && camera) {
+            // Fit camera to object: https://discourse.threejs.org/t/camera-zoom-to-fit-object/936/3
+            const offset = 1.25;
+            const boundingBox = new Box3();
+            boundingBox.setFromObject(selectedElement); // get bounding box of object
+            const center = boundingBox.getCenter(new Vector3(0, 0, 0));
+            const size = boundingBox.getSize(new Vector3(0, 0, 0));
+            // get the max side of the bounding box (fits to width OR height as needed )
+            const maxDim = Math.max(size.x, size.y, size.z);
+            const fov = camera.fov * (Math.PI / 180);
+            let cameraZ = Math.abs(maxDim / 4 * Math.tan(fov * 2));
+            cameraZ *= offset; // zoom out a little so that objects don't fill the screen
+            camera.position.z = cameraZ;
+            camera.updateProjectionMatrix();
+            if(controls) {
+                controls.target = center;
+            }else {
+                camera.lookAt(center)
+            }
+            // controls.target = selectedElement.position
+        }
+    }, [selectedElement]);
 
     return (
         <div className={styles.scene}>
@@ -17,14 +46,14 @@ export default function Test() {
                 <PerspectiveCamera makeDefault={true} ref={cameraRef} position={[3, 4, 10]} />
                 <ambientLight />
                 <pointLight castShadow={true} intensity={100} position={[3, 4, 10]} />
-                <Select multiple box>
-                    <Cube name={"cube-pink"} color={"#ff00ff"} position={[0,0,0]} />
-                    <Cube name={"cube-yellow"}color={"#ffff00"} position={[1.5,0,0]} />
-                    <Cube name={"cube-blue"} color={"#00ffff"} position={[-1.5,0,0]} />
-                    <Cube name={"cube-white"} color={"#f0f0f0"} position={[0,0,1.5]} />
+                <Select multiple box onChangePointerUp={(s) => setSelectedElement(s[0])}>
+                    <Cube name={"cube-pink"} color={"#ff00ff"} position={[0, 0, 0]} />
+                    <Cube name={"cube-yellow"} color={"#ffff00"} position={[5, 0, 0]} />
+                    <Cube name={"cube-blue"} color={"#00ffff"} position={[-5, 0, 0]} />
+                    <Cube name={"cube-white"} color={"#f0f0f0"} position={[0, 0, 5]} />
                 </Select>
                 {ENABLE_FLOOR && <Floor args={[1, 1]} />}
-                <OrbitControls camera={cameraRef.current} />
+                <OrbitControls ref={controlsRef} camera={cameraRef.current} />
             </Canvas>
         </div>
     )
